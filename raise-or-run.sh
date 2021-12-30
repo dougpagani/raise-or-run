9db06218c35a3c95dd823212672d5186fb7bf9d6
dougpagani <redstonefreedom@gmail.com>
2021-12-29 21:07:51 -0500

    add main wrapper

--- raise-or-run.sh
+++ raise-or-run.sh
@@ -17,38 +17,41 @@
 # can quickly lead to needing to spend clock cycles looking for a specific
 # instance of the application.
 
-if [[ $# -eq 0 ]]
-then
-    raise_target=$(zenity --entry --text "Raise Application:")
-    wmctrl -xa $raise_target || \
-	wmctrl -a $raise_target &
-fi
+main() {
+    if [[ $# -eq 0 ]]
+    then
+        raise_target=$(zenity --entry --text "Raise Application:")
+        wmctrl -xa $raise_target || \
+        wmctrl -a $raise_target &
+    fi
 
-if [[ $# -eq 2 ]]
-then
-    raise_target=$1
-    run_target=$2
+    if [[ $# -eq 2 ]]
+    then
+        raise_target=$1
+        run_target=$2
 
-    # target by class (-xa), then by title (-a)
-    wmctrl -xa $raise_target || \
-	wmctrl -a $raise_target || \
-	$run_target &
-fi
+        # target by class (-xa), then by title (-a)
+        wmctrl -xa $raise_target || \
+        wmctrl -a $raise_target || \
+        $run_target &
+    fi
 
-# Sometimes you want to specify opening a specific window instance
-# of an application if it is open.
-# Eg. open the add window in anki if it exists, if not then open the
-# default window if it exists, and if not then launch anki.
-if [[ $# -eq 3 ]]
-then
-    raise_target_1=$1
-    raise_target_2=$2
-    run_target=$3
+    # Sometimes you want to specify opening a specific window instance
+    # of an application if it is open.
+    # Eg. open the add window in anki if it exists, if not then open the
+    # default window if it exists, and if not then launch anki.
+    if [[ $# -eq 3 ]]
+    then
+        raise_target_1=$1
+        raise_target_2=$2
+        run_target=$3
 
-    # target by class (-xa), then by title (-a)
-    wmctrl -xa $raise_target_1 || \
-	wmctrl -a $raise_target_1 || \
-	wmctrl -xa $raise_target_2 || \
-	wmctrl -a $raise_target_2 || \
-	$run_target &
-fi
+        # target by class (-xa), then by title (-a)
+        wmctrl -xa $raise_target_1 || \
+        wmctrl -a $raise_target_1 || \
+        wmctrl -xa $raise_target_2 || \
+        wmctrl -a $raise_target_2 || \
+        $run_target &
+    fi
+}
+main
